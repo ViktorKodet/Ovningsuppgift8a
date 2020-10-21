ef80a37032e6d86f0915d2cb52e54dc2af7fe6e1
Viktor Kodet <viktor.kodet@gmail.com>
2020-10-21 14:51:29 +0200

    xedxeaewa

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("exde");
+        System.out.println("exde4e4e4");
         System.out.println("sekrjhal");
     }
 }
